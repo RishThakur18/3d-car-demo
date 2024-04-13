69b3aad807dd4cb0d47116f371a6f68c7985d153
Rishabh Singh <rishthakur18@gmail.com>
2024-04-13 20:17:43 +0530

    reset

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React, {Suspense} from 'react';
 import ReactDOM from 'react-dom/client';
 import { mainRouter } from './routes';
+import CarShowMain from "./theme/carshow/carShowMain";
 
 import "./main.css";
-import { RouterProvider } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <RouterProvider router={mainRouter} />
+      <CarShowMain />
   </React.StrictMode>,
 )
--- src/routes.jsx
+++ src/routes.jsx
@@ -7,9 +7,7 @@ import Globe from "./theme/3dglobe/globe";
 
 export const mainRouter = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<RootLayout />}>
-      <Route path="/car" index element={<CarShowMain />} />
-      <Route path="/globe" index element={<Globe />} />
+    <Route path="/" element={<CarShowMain />}>
     </Route>
   )
 );
